9229cf7b92d98a67137c4e82d52d8c15edcf655a
JoaoTheBestt <jv10vitcor@gmail.com>
2023-10-10 12:49:57 -0300

    Docs

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const header = document.querySelector("header");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", this.window.scrollY > 120);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navList = document.querySelector('.navList');
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-bx');
+    navList.classList.toggle('active');
+}
